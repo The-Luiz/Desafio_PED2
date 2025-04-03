4711c610baf7d3c6e9bce1742e7f8b939d42c30d
Luis <luiskz1212@gmail.com>
2025-04-03 17:15:41 -0600

    Add files via upload

---
+++ Sistema de Tienda en Línea con Facturación Electrónica/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_de_Tienda_en_Línea_con_Facturación_Electrónica
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
