e953526b43039244f432bce20ccb7850cef7f895
The-Luiz <luiskz1212gmail.com>
2025-04-05 15:09:32 -0600

    Agregue la clase Producto

---
+++ Sistema de Tienda en Línea con Facturación Electrónica/ClaseProducto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Tienda_en_Línea_con_Facturación_Electrónica
+{
+    internal class ClaseProducto
+    {
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+        public int Stock { get; set; }
+        public int Codigo { get; set; }
+        public string Categoria { get; set; }
+
+        public ClaseProducto(string NuevoNombre, int NuevoPrecio, int NuevoStock,int NuevoCodigo, string NuevaCategoria) 
+        {
+           Nombre = NuevoNombre;
+           Precio = NuevoPrecio;
+           Stock = NuevoStock;
+           Codigo = NuevoCodigo;
+           Categoria = Categoria;
+        }
+    }
+}
