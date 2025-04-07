8bb11eb9138d4159d732c5d804145b71e2eecf97
Moisés177 <RT240493@alumno.udb.edu.sv>
2025-04-07 17:26:28 -0600

    cambios formulario

--- Sistema de Tienda en Línea con Facturación Electrónica/FormAgregarProductos.cs
+++ Sistema de Tienda en Línea con Facturación Electrónica/FormAgregarProductos.cs
@@ -16,5 +16,11 @@ namespace Sistema_de_Tienda_en_Línea_con_Facturación_Electrónica
         {
             InitializeComponent();
         }
+
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            Form1 form1  = new Form1();
+            form1.Show();
+        }
     }
 }
