3af3163fdc42983e1cf720f68dd8b7582be617b7
The-Luiz <luiskz1212gmail.com>
2025-04-08 09:02:33 -0600

    Nuevo

--- Sistema de Tienda en Línea con Facturación Electrónica/FormStockCritico.Designer.cs
+++ Sistema de Tienda en Línea con Facturación Electrónica/FormStockCritico.Designer.cs
@@ -52,9 +52,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Berlin Sans FB", 14F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(286, 58);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 20);
+            this.label1.Size = new System.Drawing.Size(157, 31);
             this.label1.TabIndex = 1;
             this.label1.Text = "Stock Critico";
             // 
